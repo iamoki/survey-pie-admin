ceb6439d54a3d6bf9cbd4ffd452bc73765b67bea
iamoki <pureaoh@gmail.com>
2022-07-30 15:49:40 +0900

    ✅ antd 설치, 기본 레이아웃 세팅

---
+++ src/layouts/mainLayout.js
@@ -0,0 +1,33 @@
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Sider } = Layout;
+
+function MainLayout({ children }) {
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider>
+        <div
+          className="logo"
+          style={{
+            height: '32px',
+            margin: '16px',
+            background: 'rgba(255, 255, 255, 0.3)',
+          }}
+        />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1">설문 조사 관리</Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header />
+        <Content>{children}</Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default MainLayout;
---
+++ src/pages/ListPage.js
@@ -0,0 +1,7 @@
+import MainLayout from '../layouts/mainLayout';
+
+function ListPage() {
+  return <MainLayout>ListPage</MainLayout>;
+}
+
+export default ListPage;
